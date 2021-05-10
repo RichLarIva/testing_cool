2c2969802790ffdf1af7f66e5c419515f65f2a90
bootleg-cool <richard.larsson03@hotmail.se>
2021-05-10 10:48:15 +0200

    initial commit

---
+++ JS/javascript.js
@@ -0,0 +1,5 @@
+bildArray = [
+"https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.allieddomecqplc.com%2Fwp-content%2Fuploads%2F2018%2F01%2Fhelsinki2.jpg&f=1&nofb=1",
+"https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.bgviaggi.com%2Fimages%2FRovaniemi_2.jpg&f=1&nofb=1",
+"https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.lapland.fi%2Fuploads%2F2018%2F03%2Flapland-kemi-finland-snow-castle-winter-kemi-tourism-ltd.jpg&f=1&nofb=1",
+]
